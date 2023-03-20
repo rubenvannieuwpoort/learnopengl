20cbef7bfaa5bac0cbd83eab0dda976afe799712
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2023-03-20 12:44:13 +0100

    Add error callback

--- main.c
+++ main.c
@@ -3,7 +3,13 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-int main(void) { 
+void error_callback(int error, const char* description) {
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+int main(void) {
+	glfwSetErrorCallback(error_callback);
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
